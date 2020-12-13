d072046eff7db1b9efa0dcae6853d944f2ce6549
Seongmin <lll2slll@naver.com>
2020-12-13 21:07:20 +0900

    chore: lint 설정

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: ['airbnb-base', 'plugin:prettier/recommended'],
+    rules: {},
+    env: {
+        node: true,
+        es6: true,
+        mocha: true,
+    },
+};
